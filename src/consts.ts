0cf55356ad2dbdc9b93e2a412212e6cc1b74886a
801 Txchii <sdgaletech@gmail.com>
2023-04-28 16:18:50 -0700

    u

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'The Extra Bite';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'The official website of The Extra Bite';
